125396c1edba0791b5c1cb5bb120ea5faa5ef8dd
HarchenkoKat <harchen14@gmail.com>
2019-12-04 23:58:28 +0200

    final local

---
+++ models.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, DateTime
+
+Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String(20), nullable=False)
+    age = Column(Date, nullable=False)
+    weight = Column(Integer, nullable=False)
+    activity = Column(Integer, nullable=False)
+
+
+class Complex(Base):
+    __tablename__ = 'complex'
+
+    complex_name = Column(String(20), primary_key=True)
+
+class User_do_complex(Base):
+    __tablename__ = 'user_do_complex'
+
+    user_id = Column(Integer, ForeignKey('users.user_id'), primary_key=True)
+    complex_name = Column(String(20), ForeignKey('complex.complex_name'), primary_key=True)
+    time_start = Column(DateTime, primary_key=True)
+    status = Column(String(20), nullable=False)
